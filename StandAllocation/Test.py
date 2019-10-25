098a180a3a5542995e2fd1d46042cb4c9adf3242
Sujoy.Mukherjee <sujoy.mukherjee@sita.aero>
2019-10-25 10:07:01 +0530

    Structure change

---
+++ StandAllocation/Test.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import re
+from dateutil import parser
+from sklearn.preprocessing import LabelEncoder
+
+df = pd.read_excel('C:/Users/Sujoy.Mukherjee/Python/StandAllocation/BEG.xlsx')
+
+df.head()
